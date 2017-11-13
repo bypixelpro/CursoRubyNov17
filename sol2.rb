dc6955adb909e4d07de434df3d40de2a73131061
bypixelpro <info@pixelpro.es>
2017-11-13 10:46:17 +0100

    Create sol2.rb

---
+++ sol2.rb
@@ -0,0 +1,11 @@
+# 1) hay que obtener la media de estos cuatro números
+# 2) Crea un programa que pida al usuario los 4 números y muestrale la media
+
+
+puts "Introduzca 4 números"
+a = gets.to_f
+b = gets.to_f
+c = gets.to_f
+d = gets.to_f
+
+puts (a + b + c + d) / 4.0
