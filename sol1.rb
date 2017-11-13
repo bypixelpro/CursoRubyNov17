d3809c4107280bd49159d9e19a6a95493f27fe7a
bypixelpro <info@pixelpro.es>
2017-11-13 10:45:40 +0100

    Create sol1.rb

---
+++ sol1.rb
@@ -0,0 +1,4 @@
+puts 'Cuando escribes \t en un string con comillas dobles se muestra asi:'
+puts "David\tPiqué"
+puts 'Cuando escribes \n en un string con comillas dobles, se muestra asi:'
+puts "David\nPiqué"
